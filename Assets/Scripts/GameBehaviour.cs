1d5a22d147d25750569d59f322f3d2744abc4c75
Lilli <lillichuawiwat@gmail.com>
2024-04-12 09:29:26 +1000

    Bring UI

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -14,7 +14,7 @@ public class GameBehaviour : Behaviour //inherits from
     protected static SeagullController _SC { get { return SeagullController.INSTANCE; } }
     protected static ItemSpawner _IS { get { return ItemSpawner.INSTANCE; } }
 
-    protected static VFXMannager _VFXM { get { return VFXMannager.INSTANCE; } }
+    protected static VFXManager _VFXM { get { return VFXManager.INSTANCE; } }
     protected static MainMenu _MM { get { return MainMenu.INSTANCE; } }
     protected static UIManager _UI { get { return UIManager.INSTANCE; } }
 
