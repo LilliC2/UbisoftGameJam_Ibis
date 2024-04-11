7b55e0cb0bc54137ba61e397a4923ac1f8fb6d97
SkylarJudd <136395161+SkylarJudd@users.noreply.github.com>
2024-04-12 09:52:55 +1000

    Fixed bugs

--- Assets/Scripts/VFXMannager.cs
+++ Assets/Scripts/VFXMannager.cs
@@ -8,18 +8,18 @@ public class VFXManager : Singleton<VFXManager>
 
     public void SpawnParticle(int particleID, Transform location)
     {
-        print("Spawn Partical Called");
-       // if (particleID < 0 || particleID >= vfxList.Count || vfxList[particleID] == null)
-        //{
-           // Debug.LogError("Invalid particle ID or particle system is null.");
-            //return;
-       // }
+        
+       if (particleID < 0 || particleID >= vfxList.Count || vfxList[particleID] == null)
+        {
+            Debug.LogError("Invalid particle ID or particle system is null.");
+            return;
+       }
 
-       // ParticleSystem particleSystemInstance = Instantiate(vfxList[particleID], location.position, location.rotation);
-        //particleSystemInstance.transform.parent = location; // Make the particle system a child of the provided transform
-       // particleSystemInstance.Play();
+       ParticleSystem particleSystemInstance = Instantiate(vfxList[particleID], location.position, location.rotation);
+        particleSystemInstance.transform.parent = location; // Make the particle system a child of the provided transform
+        particleSystemInstance.Play();
 
-        // Assuming the particle system has a main module and it's not null
-       // Destroy(particleSystemInstance.gameObject, particleSystemInstance.main.duration);
+        //Assuming the particle system has a main module and it's not null
+        Destroy(particleSystemInstance.gameObject, particleSystemInstance.main.duration);
     }
 }
